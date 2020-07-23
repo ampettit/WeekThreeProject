336b8e35020a74274579a4cb13eac9c2f565f9b1
rnjmur <66527652+SCCC-rnjmur@users.noreply.github.com>
2020-07-22 19:00:35 -0500

    Update mCode.java

--- src/morsecode/mCode.java
+++ src/morsecode/mCode.java
@@ -6,12 +6,20 @@ import java.util.Scanner;
 public class mCode {
 	
 	private static void readTree(MorseCodeTree mtree) {
+		/**
+		 * Read in morse code from file and add to MorseCodeTree
+		 * 
+		 * @param: MorseCodeTree	The tree storing the morsecode
+		 */
+		// Scanner for reading file
 		Scanner scnr = null;
+		// try to open file - if not found catch file not found exception
 		try {
 			scnr = new Scanner(new File("Morse_Code.txt"));
 		} catch(FileNotFoundException exception) {
 			System.out.println("File not found");
 		}
+		// Read in each line from file and add to MorseCodeTree
 		while (scnr.hasNextLine()) {
 			String data = scnr.nextLine().trim();
 			if(data.length() > 0) {
@@ -22,22 +30,30 @@ public class mCode {
 	}
 	
 	public static void main(String[] args){
+		/**
+		 * Main program
+		 */
 		MorseCodeTree mtree = new MorseCodeTree();
-	
+		
+		// call readTree method
 		readTree(mtree);
-	
+		
+		// Scanner to get input from user
 		Scanner scnr = new Scanner(System.in);
-	
-		System.out.println("What would you like to be translated? \n");
+		
+		// Display question and get input from user
+		System.out.println("What would you like to be translated?");
+		System.out.println("Enter either lowercase letters or Morse code. \n");
 		String choice = scnr.nextLine().trim();
 		
-
+		// If there is a . or - in the input string do code to letter conversion
 		if(choice.contains("-") || choice.contains(".")){
 			String[] choice_split = choice.split(" ");
 			for(int i = 0; i < choice_split.length; i++){
     			System.out.print(mtree.getLetter(choice_split[i]));
 			}
 		}
+		// Otherwise do letter to code conversion
 		else{
 			for(int i = 0; i < choice.length(); i++){
     			System.out.print(mtree.getCode(choice.charAt(i)) + " ");
